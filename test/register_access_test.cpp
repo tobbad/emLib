3ddaf03525577b590080fee59dba8734714ec52c
Tobias Badertscher <badi@baerospace.ch>
2018-02-11 19:59:33 +0100

    Added test for 16 and 32 bit register set.

--- test/register_access_test.cpp
+++ test/register_access_test.cpp
@@ -6,7 +6,8 @@
 //CppUTest includes should be after your and system includes
 #include "CppUTest/TestHarness.h"
 
-uint32_t regField[10]={0};
+static const uint8_t RFSIZE=10;
+uint32_t regField[RFSIZE];
 
 /***************************************************************
  *
@@ -15,11 +16,15 @@ uint32_t regField[10]={0};
  **************************************************************/
 TEST_GROUP(REGISTER_ACCESS) {
 
+    uint32_t RFDEFAULT32 = 0xFFFFFFFF;
+    uint16_t RFDEFAULT16 = 0xFFFF;
+    uint8_t RFDEFAULT8 = 0xFF;
 
     void setup()
     {
-
-
+        for (uint8_t i;i<RFSIZE;i++) {
+            regField[i] = RFDEFAULT32;
+        }
     }
 
     void teardown()
@@ -29,10 +34,25 @@ TEST_GROUP(REGISTER_ACCESS) {
 
 };
 
-TEST(REGISTER_ACCESS, U8REG_READ){
-    static const uint32_t EXP = 342;
-	mcal::reg::access<std::uint32_t*, std::uint8_t, regField, EXP>::reg_set();
-    CHECK_EQUAL(EXP, regField[0]);
+TEST(REGISTER_ACCESS, U8REG_SET){
+    static const uint32_t EXP = 42;
+    mcal::reg::access<std::uint32_t*, std::uint8_t, regField, EXP>::reg_set();
+    CHECK_EQUAL(EXP, reinterpret_cast<uint8_t*>(regField)[0]);
+    CHECK_EQUAL(RFDEFAULT8, reinterpret_cast<uint8_t*>(regField)[1]);
+}
+
+TEST(REGISTER_ACCESS, U16REG_SET){
+    static const uint32_t EXP = 1642;
+    mcal::reg::access<std::uint32_t*, std::uint16_t, regField, EXP>::reg_set();
+    CHECK_EQUAL(EXP, reinterpret_cast<uint16_t*>(regField)[0]);
+    CHECK_EQUAL(RFDEFAULT16, reinterpret_cast<uint16_t*>(regField)[1]);
+}
+
+TEST(REGISTER_ACCESS, U32REG_SET){
+    static const uint32_t EXP = 123121642;
+    mcal::reg::access<std::uint32_t*, std::uint32_t, regField, EXP>::reg_set();
+    CHECK_EQUAL(EXP, reinterpret_cast<uint32_t*>(regField)[0]);
+    CHECK_EQUAL(RFDEFAULT32, reinterpret_cast<uint32_t*>(regField)[1]);
 }
 
 
